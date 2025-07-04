c08ad5aec941b7f73eb70f28325cf79909f0f214
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-05 00:16:03 +0545

    book web

---
+++ booksearch/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../component/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+       <Navbar />
+    </div>
+  )
+}
+
+export default Home
