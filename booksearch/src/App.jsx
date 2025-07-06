52004335e436db32d27585e4c373b125bca89fe3
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-06 17:09:33 +0545

    book web

--- booksearch/src/App.jsx
+++ booksearch/src/App.jsx
@@ -3,11 +3,15 @@ import Navbar from './component/Navbar'
 import { createBrowserRouter, Route, Router, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
+import BookDetials from './component/BookDetails'
+import Errorpage from './pages/Errorpage'
 
 const myroutes = createBrowserRouter([
   {path: '/', Component: Home},
   {path: '/home', Component: Home},
   {path: '/about', Component:About},
+  {path:'/book/:id', Component:BookDetials},
+  {path:"*",Component:Errorpage}
 ])
 const App = () => {
   
