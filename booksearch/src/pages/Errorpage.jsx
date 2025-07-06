3f44362d375bf21573f666e90cca36ce1c57455f
nikesh chaudhari <info.nikesh12@gmail.com>
2025-07-06 17:09:51 +0545

    book web

---
+++ booksearch/src/pages/Errorpage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Errorpage = () => {
+  return (
+    <div>
+           <div className="text-center mt-20">
+            <h1 className="text-4xl font-bold text-red-600">404 - Page Not Found</h1>
+            <p className="mt-4 text-gray-500">The page you are looking for does not exist.</p>
+        </div>
+    </div>
+  )
+}
+
+export default Errorpage
